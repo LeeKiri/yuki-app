43c164acd6a94af387cdbf2d99235d9f43a6d6d3
Lee Kiri <leekiritempleton@gmail.com>
2021-04-08 14:51:10 +1000

    API routes create and read done

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -12,16 +12,93 @@ app.post("/api/cat", ({ body }, res) => {
     });
 });
 
-//read
-app.post("/api/transaction/bulk", ({ body }, res) => {
-  Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+app.post("/api/user", ({ body }, res) => {
+  User.create(body)
+    .then(dbUser => {
+      res.json(dbUser);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
+
+app.post("/api/vet", ({ body }, res) => {
+  Vet.create(body)
+    .then(dbVet => {
+      res.json(dbVet);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+app.post("/api/daily", ({ body }, res) => {
+  Daily.create(body)
+    .then(dbDaily => {
+      res.json(dbDaily);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+app.post("/api/medication", ({ body }, res) => {
+  Medication.create(body)
+    .then(dbMedication => {
+      res.json(dbMedication);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+//read
+app.get("/user/all", (req, res) => {
+  db.User.find({}, (err, data) => {
+    if (error) {
+        res.send(error);
+      } else {
+        res.json(data);
+      }
+    });
+  });
+app.get("/cat/all", (req, res) => {
+  db.Cat.find({}, (err, data) => {
+    if (error) {
+        res.send(error);
+      } else {
+        res.json(data);
+      }
+    });
+  });
+app.get("/medication/all", (req, res) => {
+  db.Medication.find({}, (err, data) => {
+    if (error) {
+        res.send(error);
+      } else {
+        res.json(data);
+      }
+    });
+  });
+app.get("/daily/all", (req, res) => {
+  db.Daily.find({}, (err, data) => {
+    if (error) {
+        res.send(error);
+      } else {
+        res.json(data);
+      }
+    });
+  });
+app.get("/vet/all", (req, res) => {
+  db.Vet.find({}, (err, data) => {
+    if (error) {
+        res.send(error);
+      } else {
+        res.json(data);
+      }
+    });
+  });
+
 //update
 
 //delete
