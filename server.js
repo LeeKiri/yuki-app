3650be31efdcb95fe8a91cfe6bbf4376efeb2d78
Lee Kiri <leekiritempleton@gmail.com>
2021-05-04 10:51:33 +1000

    cookie parser corrected

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.static("public"));
 app.use(cors({ credentials: true }));
-app.use(cookieParser);
+app.use(cookieParser());
 app.use(
   session({
     secret: "keyboard cat",
